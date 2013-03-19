dad60d3da6875549a47c6483a19109fce0744bfe
Hendrickx Eli <eli.hendrickx@immalle.be>
2013-03-19 09:31:21 +0100

    1e versie :>

---
+++ Memorygame1/Memorygame1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+   
+
+ 
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        For i = 0 To 9
+            For j = 0 To 9
+                Dim lbl As New Label()
+                lbl.Name = "lbl" & i
+                lbl.Size = New Size(20, 20)
+                lbl.Location = New Point(30 * i, 30 * j)
+                lbl.BackColor = Color.DeepSkyBlue
+                Me.Controls.Add(lbl)
+
+            Next
+        Next
+    End Sub
+End Class
